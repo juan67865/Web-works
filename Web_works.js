00368d423baec309f99f3a02d5e496e573e31627
juan67965 <jcuenca129431@gmail.com>
2023-05-10 22:50:21 -0400

    change

---
+++ Web_works.js
@@ -0,0 +1,29 @@
+// In your own terms, define the following terms:
+
+// What is HTTP ?
+// Hyper Text Transfer Protocol is how data is sent or received from a server.
+// What is a URL ?
+// The URL or Uniform Resource Locator is an address for some internet resource.
+// What is DNS ?
+// The Domain Name System is a system that takes human readable URLs and converts them into IP addresses.
+// What is a query string ?
+// A query string allows you to pass a key-value pair into the URL in the format
+// ?key1=value$key2=value2
+// What are two HTTP verbs and how are they different ?
+// GET - get data from the server
+// POST - sends some data to the server
+// What is an HTTP request ?
+// An HTTP request is a request from a client to a server which follows the HTTP protocol.
+// What is an HTTP response ?
+// The HTTP response is a response from the server to the client which follows the HTTP protocol.
+// What is an HTTP header ? Give a couple examples of request and response headers you have seen.
+// The HTTP header provides additional information about the request or response. 
+// An example of request headers can be: User-agent, Accept, and Cookie.
+// An example of response headers can be: Last-Modified, Content-Type, and Cache-Control.
+// What are the processes that happen when you type 
+//“[http://somesite.com/some/page.html](http://somesite.com/some/page.html)” into a browser?
+// 1. The browser "resolves" the URL name into a IP address using DNS.
+// 2. The browser makes a request to the IP address.
+// 3. The server sends a response.
+// 4. The browser makes  DOM from that HTML and finds any other resources needed.
+// 5. The browser makes a separate HTTP request for those resources and receives response from the server for each.
